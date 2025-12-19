5ff2266fcead95a9d8035529e75097aefd8d15ad
jmssnr <maussner.johannes@gmail.com>
2025-12-19 11:56:58 +0100

    fix styling of area charts

--- components/chart/colors.ts
+++ components/chart/colors.ts
@@ -1,8 +1,8 @@
 export const chartColors = {
-  violet: { fill: "fill-violet-900", stroke: "stroke-violet-500" },
-  amber: { fill: "fill-amber-900", stroke: "stroke-amber-500" },
-  pink: { fill: "fill-pink-900", stroke: "stroke-pink-500" },
-  blue: { fill: "fill-blue-900", stroke: "stroke-blue-500" },
+  violet: { fill: "fill-violet-900/20", stroke: "stroke-violet-500" },
+  amber: { fill: "fill-amber-900/20", stroke: "stroke-amber-500" },
+  pink: { fill: "fill-pink-900/20", stroke: "stroke-pink-500" },
+  blue: { fill: "fill-blue-900/20", stroke: "stroke-blue-500" },
 } as const;
 
 export type ChartColor = keyof typeof chartColors;
